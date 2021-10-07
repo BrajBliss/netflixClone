e3a1493246c75ec44a5ffb943e6b7732b3f7e42b
Amay <brajbliss@gmail.com>
2021-10-07 14:19:44 +0530

    featured

--- frontEnd(client)/src/home/Home.jsx
+++ frontEnd(client)/src/home/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
 import './home.scss';
 import Navbar from '../components/navbar/Navbar';
+import Featured from '../components/featured/Featured';
 
 const Home = () => {
 	return (
 		<div className='home'>
 			<Navbar />
+			<Featured />
 		</div>
 	);
 };
