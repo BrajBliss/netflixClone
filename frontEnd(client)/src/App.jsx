e8c63cb2de70faa10bd203084f23086bd95766e2
Amay <brajbliss@gmail.com>
2021-10-07 13:22:19 +0530

    navbar and home

---
+++ frontEnd(client)/src/App.jsx
@@ -0,0 +1,8 @@
+import './app.scss';
+import Home from './home/Home';
+
+const App = () => {
+	return <Home />;
+};
+
+export default App;
---
+++ frontEnd(client)/src/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './home.scss';
+import Navbar from '../components/navbar/Navbar';
+
+const Home = () => {
+	return (
+		<div className='home'>
+			<Navbar />
+		</div>
+	);
+};
+
+export default Home;
