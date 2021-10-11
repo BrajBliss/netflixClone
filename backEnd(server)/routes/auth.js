3a9cdaf6dc9fc9357a3537e2b0f6adc8078c0992
Amay <brajbliss@gmail.com>
2021-10-11 15:55:26 +0530

    login with JWT

--- backEnd(server)/routes/auth.js
+++ backEnd(server)/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
 // Register
 router.post('/register', async (req, res) => {
@@ -17,6 +18,38 @@ router.post('/register', async (req, res) => {
 	try {
 		const user = await newUser.save();
 		res.json(user);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+// Login
+router.post('/login', async (req, res) => {
+	const { email } = req.body;
+	try {
+		const user = await User.findOne({ email: email });
+		!user && res.status(401).json('Wrong password or username!');
+
+		const bytes = CryptoJS.AES.decrypt(
+			user.password,
+			process.env.SECRET_KEY
+		);
+		const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+		originalPassword !== req.body.password &&
+			res.status(401).json('Wrong password or username!');
+
+		const accessToken = jwt.sign(
+			{
+				id: user._id,
+				isAdmin: user.isAdmin,
+			},
+			process.env.SECRET_KEY,
+			{ expiresIn: '1d' }
+		);
+
+		const { password, ...info } = user._doc;
+		res.status(200).json({ ...info, accessToken });
 	} catch (err) {
 		res.status(500).json(err);
 	}
