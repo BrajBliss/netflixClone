9f61d4eaf369819fab0f0986434ab8b35cb9e13e
Amay <brajbliss@gmail.com>
2021-10-11 18:37:29 +0530

    movie crud

--- backEnd(server)/index.js
+++ backEnd(server)/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
+const movieRoute = require('./routes/movies');
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.use(express.json());
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
+app.use('/api/movies', movieRoute);
 
 app.listen(8800, () => {
 	console.log('BackEnd server is running');
