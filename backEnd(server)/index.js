9bd1069bcea7a1251578566016774af47c06de00
Amay <brajbliss@gmail.com>
2021-10-12 18:04:09 +0530

    list crud

--- backEnd(server)/index.js
+++ backEnd(server)/index.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const movieRoute = require('./routes/movies');
+const listRoute = require('./routes/lists');
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/movies', movieRoute);
+app.use('/api/lists', listRoute);
 
 app.listen(8800, () => {
 	console.log('BackEnd server is running');
